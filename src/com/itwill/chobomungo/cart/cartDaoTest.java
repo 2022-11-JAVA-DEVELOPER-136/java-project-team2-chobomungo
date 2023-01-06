33ef632b45b95b46a862377bfd039d7e63b994d6
21HO <doki0804@naver.com>
2023-01-06 13:35:55 +0900

    ..

--- src/com/itwill/chobomungo/cart/cartDaoTest.java
+++ src/com/itwill/chobomungo/cart/cartDaoTest.java
@@ -6,17 +6,7 @@ public class cartDaoTest {
 		CartDao cartDao = new CartDao(); 
 		
 		
-		// 1. insert
-		int rowCount = cartDao.insertCart(3, "book1", 6); 
-		System.out.println(rowCount);
-		
-		//2.update
-		rowCount = cartDao.updateCart(2, "book1", 6);
-		System.out.println(rowCount);
-		
-		//3.delete
-		rowCount = cartDao.deleteCart("book1", 6);
-		System.out.println(rowCount);
+
 		
 		
 	}
