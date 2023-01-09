9c1aaba777b44df2477696200a90c3f0f1df9e91
doki0804 <doki0804@naver.com>
2023-01-09 22:10:32 +0900

    ..

--- src/com/itwill/chobomungo/ui/이원호/OrderPanel.java
+++ src/com/itwill/chobomungo/ui/이원호/OrderPanel.java
@@ -4,41 +4,56 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.util.List;
+
 import javax.swing.JCheckBox;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
+
+import com.itwill.chobomungo.order.OrderService;
+import com.itwill.chobomungo.order.Orders;
+
 import javax.swing.JScrollPane;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 public class OrderPanel extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JLabel orderNumberOfItemtLB;
+	private JTextField orderDeliveryAddressTF;
+	private JTextField orderDeliveryNameTF;
+	private JPanel orderDetailPanel;
+	private JPanel orderListPanel;
 
 	/**
 	 * Create the panel.
+	 * @throws Exception 
 	 */
-	public OrderPanel() {
+	public OrderPanel() throws Exception {
+		setPreferredSize(new Dimension(400, 620));
+		/*********/
+		OrderService orderService = new OrderService(); 
+		
+		/************/
 		setLayout(new BorderLayout(0, 0));
 		
 		JPanel orderPanel = new JPanel();
+		orderPanel.setLayout(null);
+		orderPanel.setBackground(Color.WHITE);
 		add(orderPanel, BorderLayout.CENTER);
-		orderPanel.setLayout(new BorderLayout(0, 0));
-		
-		JPanel orderPanel_1 = new JPanel();
-		orderPanel_1.setLayout(null);
-		orderPanel_1.setBackground(Color.WHITE);
-		orderPanel.add(orderPanel_1, BorderLayout.CENTER);
 		
 		JPanel orderDeliveryPanel = new JPanel();
 		orderDeliveryPanel.setLayout(null);
 		orderDeliveryPanel.setBorder(null);
 		orderDeliveryPanel.setBackground(new Color(226, 226, 226));
 		orderDeliveryPanel.setBounds(12, 10, 348, 102);
-		orderPanel_1.add(orderDeliveryPanel);
+		orderPanel.add(orderDeliveryPanel);
 		
 		JLabel orderDeliveryInfoLB = new JLabel("배송정보");
 		orderDeliveryInfoLB.setFont(new Font("D2Coding ligature", Font.PLAIN, 14));
@@ -54,15 +69,15 @@ public class OrderPanel extends JPanel {
 		orderDeliveryCB.setBounds(238, 6, 102, 23);
 		orderDeliveryPanel.add(orderDeliveryCB);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(92, 36, 229, 21);
-		orderDeliveryPanel.add(textField);
+		orderDeliveryAddressTF = new JTextField();
+		orderDeliveryAddressTF.setColumns(10);
+		orderDeliveryAddressTF.setBounds(92, 36, 229, 21);
+		orderDeliveryPanel.add(orderDeliveryAddressTF);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(92, 67, 229, 21);
-		orderDeliveryPanel.add(textField_1);
+		orderDeliveryNameTF = new JTextField();
+		orderDeliveryNameTF.setColumns(10);
+		orderDeliveryNameTF.setBounds(92, 67, 229, 21);
+		orderDeliveryPanel.add(orderDeliveryNameTF);
 		
 		JLabel orderDeliveryAddressLB = new JLabel("주소");
 		orderDeliveryAddressLB.setFont(new Font("D2Coding ligature", Font.PLAIN, 14));
@@ -77,22 +92,22 @@ public class OrderPanel extends JPanel {
 		JPanel orderNumberOfItemPanel = new JPanel();
 		orderNumberOfItemPanel.setLayout(null);
 		orderNumberOfItemPanel.setBounds(12, 148, 348, 30);
-		orderPanel_1.add(orderNumberOfItemPanel);
+		orderPanel.add(orderNumberOfItemPanel);
 		
-		orderNumberOfItemtLB = new JLabel("");
+		JLabel orderNumberOfItemtLB = new JLabel("");
 		orderNumberOfItemtLB.setBounds(12, 0, 75, 30);
 		orderNumberOfItemPanel.add(orderNumberOfItemtLB);
 		
 		JButton orderBTN = new JButton("주문 하기");
 		orderBTN.setBounds(12, 429, 348, 43);
-		orderPanel_1.add(orderBTN);
+		orderPanel.add(orderBTN);
 		
 		JPanel orderTotalPricePanel = new JPanel();
 		orderTotalPricePanel.setLayout(null);
 		orderTotalPricePanel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		orderTotalPricePanel.setBackground(Color.WHITE);
 		orderTotalPricePanel.setBounds(12, 376, 348, 30);
-		orderPanel_1.add(orderTotalPricePanel);
+		orderPanel.add(orderTotalPricePanel);
 		
 		JLabel orderTotalNameLB = new JLabel("최종 결제 금액");
 		orderTotalNameLB.setFont(new Font("D2Coding ligature", Font.BOLD, 14));
@@ -107,20 +122,75 @@ public class OrderPanel extends JPanel {
 		
 		JScrollPane orderListScrollPane = new JScrollPane();
 		orderListScrollPane.setBounds(12, 178, 348, 188);
-		orderPanel_1.add(orderListScrollPane);
+		orderPanel.add(orderListScrollPane);
 		
-		JPanel orderListPanel = new JPanel();
-		orderListPanel.setPreferredSize(new Dimension(320, 200));
+		orderListPanel = new JPanel();
+		orderListPanel.setBackground(Color.WHITE);
+		orderListPanel.setPreferredSize(new Dimension(320, 400));
 		orderListScrollPane.setViewportView(orderListPanel);
+		orderListPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JPanel orderDetailPanel = new JPanel();
-		orderDetailPanel.setPreferredSize(new Dimension(320, 60));
-		orderListPanel.add(orderDetailPanel);
+		orderDetailPanel = new JPanel();
 		orderDetailPanel.setLayout(null);
+		orderDetailPanel.setPreferredSize(new Dimension(320, 80));
+		orderListPanel.add(orderDetailPanel);
 		
 		JLabel orderDetailLB = new JLabel("New label");
-		orderDetailLB.setBounds(0, 0, 320, 60);
+		orderDetailLB.setBounds(12, 10, 250, 21);
 		orderDetailPanel.add(orderDetailLB);
+		
+		JLabel orderDetailPriceLB = new JLabel("New label");
+		orderDetailPriceLB.setBounds(12, 49, 250, 21);
+		orderDetailPanel.add(orderDetailPriceLB);
+		
+		JButton orderDetailDeleteBTN = new JButton("x");
+		orderDetailDeleteBTN.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		orderDetailDeleteBTN.setBounds(274, 9, 39, 23);
+		orderDetailPanel.add(orderDetailDeleteBTN);
+	
+		displayOrderList();
+	}
 
+	public void displayOrderList() throws Exception {
+		OrderService orderService = new OrderService(); 
+		String user = "book1";
+		orderListPanel.removeAll();
+		List<Orders> orderList = orderService.orderList(user);
+		System.out.println(orderList);
+		for(Orders order : orderList) {
+			orderDetailPanel = new JPanel();
+			orderDetailPanel.setLayout(null);
+			orderDetailPanel.setPreferredSize(new Dimension(320, 80));
+			
+			JLabel orderDetailLB = new JLabel(order.getO_desc());
+			orderDetailLB.setBounds(12, 10, 250, 21);
+			orderDetailPanel.add(orderDetailLB);
+			
+			JLabel orderDetailPriceLB = new JLabel("가격 : "+new DecimalFormat("#,###원").format(order.getO_price()));
+			orderDetailPriceLB.setBounds(12, 49, 250, 21);
+			orderDetailPanel.add(orderDetailPriceLB);
+			
+			JButton orderDetailDeleteBTN = new JButton("x");
+			orderDetailDeleteBTN.setBounds(274, 9, 39, 23);
+			orderDetailPanel.add(orderDetailDeleteBTN);
+			orderDetailDeleteBTN.addActionListener(new ActionListener() {
+				private Orders o = order;
+				public void actionPerformed(ActionEvent e) {
+					// 오더리스트 1개 삭제
+					try {
+						int deleteOrderListCount = orderService.deleteOrderNo(user, o.getO_no());
+						JOptionPane.showMessageDialog(null, deleteOrderListCount + "개의 주문이 삭제되었습니다.");
+						displayOrderList();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			});
+			orderListPanel.add(orderDetailPanel);
+			
+		}
 	}
 }
