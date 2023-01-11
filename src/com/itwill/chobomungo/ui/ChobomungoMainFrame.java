e0290b8d69781803dcf4e2b4db19c4a43d474ab1
21HO <doki0804@naver.com>
2023-01-11 15:23:10 +0900

    Merge branch 'master' of https://github.com/2022-11-JAVA-DEVELOPER/java-project-team2-chobomungo.git

--- src/com/itwill/chobomungo/ui/ChobomungoMainFrame.java
+++ src/com/itwill/chobomungo/ui/ChobomungoMainFrame.java
@@ -270,7 +270,6 @@ public class ChobomungoMainFrame extends JFrame {
 		//실행시 로그인 전 불활성화 패널
 		cartPannel.setEnabled(false);
 		orderPanel.setEnabled(false);
-		userUpdatePanel.setEnabled(false);
 		productDetailPanel.setEnabled(false);
 		
 		/***********서비스객체 생성**************/
