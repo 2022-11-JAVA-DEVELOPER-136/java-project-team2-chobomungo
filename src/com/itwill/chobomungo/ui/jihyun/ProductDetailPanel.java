382864e56ab1451785358735ca2b192d9ac97481
21HO <doki0804@naver.com>
2023-01-11 16:55:22 +0900

    ..

--- src/com/itwill/chobomungo/ui/jihyun/ProductDetailPanel.java
+++ src/com/itwill/chobomungo/ui/jihyun/ProductDetailPanel.java
@@ -216,6 +216,7 @@ public class ProductDetailPanel extends JPanel {
 						int orderQty = Integer.parseInt(orderQtyStr);
 						mainFrame.orderService.create(orderQty, p_no, mainFrame.loginUser.getUserId());
 						mainFrame.orderPanel.displayOrderList();
+						productAmountCB.setSelectedIndex(0);
 						mainFrame.chobomungoTabbedPane.setSelectedIndex(3);
 						//메세지 출력
 						JOptionPane.showMessageDialog(null, "주문목록에 상품이 추가되었습니다.");
